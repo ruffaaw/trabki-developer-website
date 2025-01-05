709cd44ccd06cd98c8971af0b0e15f394358aab2
ruffaaw <rafal.p.ciupek@gmail.com>
2025-01-05 17:28:54 +0100

    feat: added home section to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
 import Header from "@/components/Header";
-
+import HomeSection from "@/components/HomeSection";
 export default function Home() {
   return (
     <div className="h-screen w-full bg-white">
       <div className="h-full w-full overflow-hidden">
         <div className="h-screen">
           <Header />
-          <div
-            className="bg-render1 bg-cover bg-center h-full w-full opacity-60"
-            data-aos="fade-in"
-            data-aos-duration="3000"
-          />
+          <HomeSection />
         </div>
       </div>
     </div>
