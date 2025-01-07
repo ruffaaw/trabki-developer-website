25fd95eca116d24d586b9405f8dd8042b2372b10
ruffaaw <rafal.p.ciupek@gmail.com>
2025-01-07 14:15:51 +0100

    style: changed background and text color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Header from "@/components/Header";
 import HomeSection from "@/components/HomeSection";
 export default function Home() {
   return (
-    <div className="h-screen w-full bg-white">
+    <div className="h-screen w-full bg-green1">
       <div className="h-full w-full overflow-hidden">
         <div className="h-screen">
           <Header />
