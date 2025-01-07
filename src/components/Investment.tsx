9510a2cef318e2ce3374387f41496e0b51091039
ruffaaw <rafal.p.ciupek@gmail.com>
2025-01-07 17:26:51 +0100

    feat: added investment section

---
+++ src/components/Investment.tsx
@@ -0,0 +1,14 @@
+export default function Investment() {
+  return (
+    <section
+      id="investment"
+      className="w-full h-full flex flex-col items-center bg-green1 relative scroll-mt-20"
+    >
+      <div>
+        <p className="text-3xl sm:text-4xl lg:text-6xl text-green4 pt-10 font-bold text-center break-words max-w-full text-black">
+          O INWESTYCJI
+        </p>
+      </div>
+    </section>
+  );
+}
