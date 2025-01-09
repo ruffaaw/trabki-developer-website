188c90dcd4c2061c706f7efb607cd695da5baaeb
ruffaaw <rafal.p.ciupek@gmail.com>
2025-01-09 19:22:21 +0100

    feat: expanded benefits section

---
+++ src/data/profitsData.tsx
@@ -0,0 +1,47 @@
+import {
+  FaTemperatureArrowUp,
+  FaHouseChimneyUser,
+  FaLeaf,
+  FaHandsHoldingChild,
+  FaHouseLaptop,
+  FaUmbrellaBeach,
+} from "react-icons/fa6";
+
+export const benefits = [
+  {
+    icon: <FaTemperatureArrowUp className="text-brown w-14 h-14 mb-4" />,
+    title: "Ogrzewanie",
+    description:
+      "W całym mieszkaniu zainstalowano ogrzewanie podłogowe, co zapewnia wyjątkowy komfort termiczny przez cały rok.",
+  },
+  {
+    icon: <FaHouseChimneyUser className="text-brown w-14 h-14 mb-4" />,
+    title: "Osiedle",
+    description:
+      "Zamknięte i monitorowane, zapewniające bezpieczeństwo i prywatność, z dostępem do dwóch miejsc parkingowych na mieszkanie i wspólnej strefy wypoczynku.",
+  },
+  {
+    icon: <FaLeaf className="text-brown w-14 h-14 mb-4" />,
+    title: "Przestrzeń zielona",
+    description:
+      "Obszerne tereny zielone otaczające osiedle, sprzyjające aktywnemu wypoczynkowi i spacerom na świeżym powietrzu.",
+  },
+  {
+    icon: <FaHandsHoldingChild className="text-brown w-14 h-14 mb-4" />,
+    title: "Komfort",
+    description:
+      "Nowoczesny piec gazowy oraz wysoki standard wykończenia mieszkań zapewniający efektywne ogrzewanie i komfort użytkowania.",
+  },
+  {
+    icon: <FaHouseLaptop className="text-brown w-14 h-14 mb-4" />,
+    title: "Udogodnienia",
+    description:
+      'Wspólna strefa zielona z altanką oraz bliskość Centrum Edukacyjno-Rekreacyjnego "Solne Miasto" z basenem i siłownią.',
+  },
+  {
+    icon: <FaUmbrellaBeach className="text-brown w-14 h-14 mb-4" />,
+    title: "Rekreacja",
+    description:
+      "Bliskość kompleksu rekreacyjnego oferującego bogatą ofertę sportową, idealna dla osób lubiących aktywny tryb życia.",
+  },
+];
