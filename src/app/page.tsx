97f191f72870546420352c5fba972731de3d0860
ruffaaw <rafal.p.ciupek@gmail.com>
2025-01-09 17:44:12 +0100

    feat: added localization section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Header from "@/components/Header";
 import HomeSection from "@/components/HomeSection";
 import Investment from "@/components/Investment";
+import Localization from "@/components/Localization";
 export default function Home() {
   return (
     <div className="h-full w-full bg-green1 overflow-hidden">
       <Header />
       <HomeSection />
       <Investment />
+      <Localization />
     </div>
   );
 }
