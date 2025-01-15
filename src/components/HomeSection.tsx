96fcf72474607b16d56f00b832a760cf51cdfeb9
ruffaaw <rafal.p.ciupek@gmail.com>
2025-01-15 15:40:36 +0100

    feat: improved logo and buttons

--- src/components/HomeSection.tsx
+++ src/components/HomeSection.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function HomeSection() {
   return (
@@ -6,35 +7,37 @@ export default function HomeSection() {
       <div
         className="bg-render1 bg-cover bg-center h-full w-full flex flex-col justify-center items-center space-y-20"
         data-aos="fade-in"
-        data-aos-duration="3000"
+        data-aos-duration="2000"
       >
         <Image
           src="/logo.png"
           alt="Logo"
-          width={300}
-          height={300}
+          width={250}
+          height={250}
           data-aos="fade-up"
           data-aos-delay="500"
           data-aos-duration="1000"
           className=" drop-shadow-md"
         />
-        <div className="flex flex-row space-x-10">
-          <button
-            className="p-3 w-56 text-color5 bg-color3 hover:bg-color2 rounded-2xl drop-shadow-xl "
+        <div className="flex max-md:flex-col max-md:space-y-5 max-md:w-full max-md:items-center md:space-x-10 mt-8">
+          <Link
+            className="py-3 w-56 max-md:w-3/5  text-color5 rounded-2xl bg-color3 hover:bg-color2 hover:text-white transition break-words max-w-full text-center drop-shadow-xl"
+            href="#houses"
             data-aos="zoom-in"
             data-aos-delay="900"
             data-aos-duration="1000"
           >
             Zobacz domy
-          </button>
-          <button
-            className="p-3 w-56 text-color5 bg-color3 hover:bg-color2 rounded-2xl shadow-xl hover:scale-110"
+          </Link>
+          <Link
+            className="py-3 w-56 max-md:w-3/5  text-color5 rounded-2xl bg-color3 hover:bg-color2 hover:text-white transition break-words max-w-full text-center drop-shadow-xl"
+            href="#contact"
             data-aos="zoom-in"
             data-aos-delay="1100"
             data-aos-duration="1000"
           >
             Kontakt
-          </button>
+          </Link>
         </div>
       </div>
     </section>
