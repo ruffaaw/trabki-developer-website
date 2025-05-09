e926d5681aedc4260d9ca22610c173dde2517be5
ruffaaw <rafalciupek07@gmail.com>
2025-05-09 22:04:37 +0200

    feat: updated AboutUs section

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -1,5 +1,3 @@
-import { FaHome, FaRulerCombined, FaMapMarkerAlt } from "react-icons/fa";
-import Image from "next/image";
 import { benefits } from "@/data/profitsData";
 
 export default function AboutUs() {
@@ -44,38 +42,8 @@ export default function AboutUs() {
         </div>
       </div>
 
-      <div className="relative w-full lg:mt-10">
-        <div className="hidden lg:block relative w-full h-[1080px] xl:h-[1000px]">
-          <Image
-            src="/render4.png"
-            alt="Render 4"
-            className="object-cover opacity-60"
-            fill
-            data-aos="zoom-out"
-            data-aos-duration="1000"
-          />
-          <div className="absolute inset-0 grid grid-cols-3 gap-6 p-5">
-            {benefits.map((benefit, index) => (
-              <div
-                key={index}
-                className="flex flex-col items-center text-center p-6 bg-white bg-opacity-80 rounded-lg shadow-lg transform hover:scale-105 transition duration-300 ease-in-out"
-                data-aos="fade-up"
-                data-aos-duration="1000"
-                data-aos-delay={benefit.delay}
-              >
-                <p className="w-14 h-14 mb-4">{benefit.icon}</p>
-                <h3 className="text-2xl xl:text-3xl font-bold text-color2 mb-3">
-                  {benefit.title}
-                </h3>
-                <p className="text-lg xl:text-xl text-color3">
-                  {benefit.description}
-                </p>
-              </div>
-            ))}
-          </div>
-        </div>
-
-        <div className="lg:hidden grid grid-cols-1 sm:grid-cols-2 gap-6 p-6 sm:p-10">
+      <div className="relative w-full ">
+        <div className=" grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-6 sm:p-10">
           {benefits.map((benefit, index) => (
             <div
               key={index}
