b5145a2e71aaaf4ee9f6735c7ce4bef92af327fd
ruffaaw <rafalciupek07@gmail.com>
2025-05-29 12:11:25 +0200

    feat: corrected status and displayed price

--- src/components/Houses.tsx
+++ src/components/Houses.tsx
@@ -27,7 +27,7 @@ export default function Houses() {
   const getStatusText = (status: number) => {
     switch (status) {
       case 0:
-        return "Niedostępny";
+        return "Sprzedany";
       case 1:
         return "Dostępny";
       case 2:
@@ -241,10 +241,12 @@ export default function Houses() {
                   <span className="font-semibold">Pokoje:</span>{" "}
                   {selectedHouse.pokoje}
                 </p>
-                <p className="text-color3">
-                  <span className="font-semibold">Cena:</span>{" "}
-                  {selectedHouse.cena.toLocaleString()} zł
-                </p>
+                {selectedHouse.status !== 0 && (
+                  <p className="text-color3">
+                    <span className="font-semibold">Cena:</span>{" "}
+                    {selectedHouse.cena.toLocaleString()} zł
+                  </p>
+                )}
                 <p className="text-color3">
                   <span className="font-semibold">Działka:</span>{" "}
                   {selectedHouse.dzialka} ara
